d089320a948a3ef34ec40f03328af0f1fbd85e77
JM991103 <ljn1103@naver.com>
2022-09-08 17:29:17 +0900

    백준_1일차

---
+++ BackJoon_Cs/BackJoon_Cs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackJoon_Cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("         ,r'\"7");
+            Console.WriteLine("r`-_   ,'  ,/");
+            Console.WriteLine(" \\. \". L_r'");
+            Console.WriteLine("   `~\\/");
+            Console.WriteLine("      |");
+            Console.WriteLine("      |");
+        }
+    }
+}
+
+
+
+
